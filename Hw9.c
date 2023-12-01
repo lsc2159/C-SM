5364b810295c59c84cf51cc7701ae0ab25e44269
lsc2159 <127822509+lsc2159@users.noreply.github.com>
2023-12-01 12:29:08 +0900

    Add files via upload

---
+++ Hw9.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int convCase(int ch){
+    const int diff = 'a'-'A';
+    if (ch>='A'&& ch<='Z')
+        return ch +diff;
+    else if(ch >='a' && ch<='Z')
+        return ch-diff;
+    else 
+        return ch;
+}
+
+int main(void){
+    char input[1000];
+    printf("Input> ");
+    fgets(input, sizeof(input), stdin);
+
+    for (int i=0; input[i] != '\0'; i++){
+        input[i]=convCase(input[i]);
+    }
+    printf("Output> %s", input);
+    return 0;
+}
